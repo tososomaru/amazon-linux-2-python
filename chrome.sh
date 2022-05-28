0fc7cdaf5f6ddd1db7204c60b7eda69191a61ebb
tososomaru <delatornikita@gmail.com>
2022-05-28 14:40:02 +0600

    Add a script to install chrome

---
+++ chrome.sh
@@ -0,0 +1,7 @@
+#!usr/bin/env bash
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+yum localinstall google-chrome-stable_current_x86_64.rpm
+
+yum upgrade google-chrome-stable
